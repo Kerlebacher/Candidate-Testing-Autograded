0db8b6d6b1becb615a162b5676e7f585508a6d6e
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-01-23 00:00:11 +0000

    testing part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,25 +5,26 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-// let question = "Who was the first American woman in space? ";
-// let correctAnswer = "Sally Ride";
-// let candidateAnswer = "";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let question = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2*10=" , "Given the array [8, 'Orbit', Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswer = ['Sally Ride', 'true', '40', 'Trajectry', 3];
-let candidateAnswer = []
+let questions = ["Who was the first American woman in space? " ,"True or false: 5 kilometer == 5000 meters? " ,"(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
-  candidateName = input.question("Enter your name:");
+  candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    candidateAnswer = input.question("Who was the first American woman in space? ");
-    correctAnswer = "Sally Ride";
+for (i = 0; i < questions.length; i++) {
+  candidateAnswers = input.questions([i]);
+}
 
 
 }
@@ -31,10 +32,10 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer = correctAnswer) {
-  console.log("Correct!");
+if (candidateAnswers = correctAnswers) {
+  console.log(` ${candidateAnswers} is Correct!`);
 } else {
-  console.log("Incorrect.");
+  console.log(` ${candidateAnswers} is Incorrect.`);
 }
 
 
